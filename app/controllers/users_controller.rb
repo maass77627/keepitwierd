c4a4590e16f8535caffc5b092e526f9a964e2a76
kelly_maass <kmaass77627@gmail.com>
2020-04-17 13:44:24 -0500

    createmethod

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,25 @@ class UsersController < ApplicationController
     end 
 
     def create 
-
+        @user = User.new(user_params)
+        if @user.save 
+            session[:user_id] = @user.id
+            redirect_to @user
+        else
+            render :new
+        end 
     end 
 
 
-    def show 
 
+    def show 
+        @user =User.find(params[:id])
+
+    end 
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:username, :email, :password)
     end 
 end
