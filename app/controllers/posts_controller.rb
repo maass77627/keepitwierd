132fb99e3b706ae42e0e59b55521c2d3c8c0d921
kelly_maass <kmaass77627@gmail.com>
2020-04-17 05:57:23 -0500

    setting up routes and actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+
+    def index
+
+    end 
+
+    def new 
+
+    end 
+
+    def create
+
+    end 
+
+    def show
+
+    end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
   resources :users
   resources :comments
   resources :posts
