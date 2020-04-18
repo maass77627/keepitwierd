555e69c76ae83e9c821ff9ce6a347032dae07da1
kelly_maass <kmaass77627@gmail.com>
2020-04-17 22:42:13 -0500

    setting up sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+
+
+    def new 
+
+    end 
+
+def create 
+
+end 
+
+def destroy 
+
+end 
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :users
   resources :comments
   resources :posts
+  get '/login', to: "sessions#new", as: "login"
+  post '/login', to: "sessions#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
