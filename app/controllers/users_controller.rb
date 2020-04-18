64032306a3699e16f8b07641eea709f83d3148b3
kelly_maass <kmaass77627@gmail.com>
2020-04-17 22:29:10 -0500

    setting up user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,10 @@ class UsersController < ApplicationController
     end 
 
     def create 
-        @user = User.new(user_params)
+        @user = User.create(user_params)
         if @user.save 
             session[:user_id] = @user.id
-            redirect_to @user
+            redirect_to posts_path
         else
             render :new
         end 
